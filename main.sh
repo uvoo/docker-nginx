18897f73fb621fe581923e440a6fe0acd22589f3
Jeremy Busk <jeremybusk@gmail.com>
2023-01-11 18:19:09 -0700

    change cmd

--- main.sh
+++ main.sh
@@ -8,7 +8,9 @@ name=nginx
 org=uvoo
 docker build --tag $org/$name:latest --tag $org/$name:$release --tag "$org/$name:$release-$os" .
 echo Push to docker repo in 5 seconds; sleep 5
-echo $DOCKERHUB_TOKEN | docker login --username $DOCKERHUB_USERNAME --password-stdin
+echo $DOCKERHUB_TOKEN | docker login --u $DOCKERHUB_USERNAME --password-stdin
+# echo $DOCKERHUB_TOKEN | docker login --username $DOCKERHUB_USERNAME --password-stdin
 docker push $org/$name:$release
 docker push $org/$name:latest
+docker push "$org/$name:$release-$os"
 docker logout
