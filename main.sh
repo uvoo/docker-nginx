cf93344596ada33eb2c0fff2aa7745afecfd9875
Jeremy Busk <jeremybusk@gmail.com>
2023-01-11 18:10:21 -0700

    update tag info

--- main.sh
+++ main.sh
@@ -2,10 +2,11 @@
 set -e
 
 echo "Build and push docker container to Dockerhub."
-release=0.1.0
-name=nginx-alpine
+os=alpine
+release="0.1.0"
+name=nginx
 org=uvoo
-docker build --tag $org/$name:latest --tag $org/$name:$release .
+docker build --tag $org/$name:latest --tag $org/$name:$release --tag "$org/$name:$release-$os" .
 echo Push to docker repo in 5 seconds; sleep 5
 echo $DOCKERHUB_TOKEN | docker login --username $DOCKERHUB_USERNAME --password-stdin
 docker push $org/$name:$release
